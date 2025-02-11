aa16dfb9b5be90beb809d8739e10eddc6585dd46
Gavin Reid <gavingq94@gmail.com>
2025-02-10 19:46:05 -0500

    project 10

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+function Header({ handleAddButtonClick }) {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">Date, Location</p>
+      <button
+        onClick={handleAddButtonClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Name</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
