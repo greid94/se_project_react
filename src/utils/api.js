62024d63529bd1db6796e9a5b326913aa2d314f4
Gavin Reid <gavingq94@gmail.com>
2025-07-10 19:41:14 -0400

    pj15

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.greidwtwr.twilightparadox.com"
+    : "http://localhost:3001";
 const getToken = () => {
   return localStorage.getItem("jwt");
 };
